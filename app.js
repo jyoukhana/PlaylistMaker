47f025abfffb0421cee8737063627d31feb55b72
John Youkhana <jyoukhana1765@gmail.com>
2020-07-18 22:08:09 -0500

    added pug template stuff + routes

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const path = require('path');
+
+//main app
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//home route
 app.get('/', function(req, res){
-  res.send('hello world')
+  res.render('index', {
+    title: "some lists"
+  })
 });
 
+//add list route
+app.get('/lists/add', function(req, res){
+  res.render('add_list', {
+    title: "add lists"
+  })
+});
+
+//listening on server
 app.listen(3000, function(){
   console.log('server started on port 3000')
-})
+});
